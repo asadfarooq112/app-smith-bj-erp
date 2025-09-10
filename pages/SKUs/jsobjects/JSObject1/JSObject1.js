bda9cf61a4c8d93febb08d5c338fbc7f72ac5bee
AI Shah <asadfarooqmb@gmail.com>
2025-09-10 13:17:36 +0000

    inventory and sku done

--- pages/SKUs/jsobjects/JSObject1/JSObject1.js
+++ pages/SKUs/jsobjects/JSObject1/JSObject1.js
@@ -50,6 +50,66 @@ export default {
 			console.log(e?.message);
 			throw(e);
 		}
+	},
+	
+	async editSKU () {
+
+		try {
+
+			if (Input1.text == 381000) {
+
+			await sku_update.run();
+
+			const payload_data = `
+			Code: ${Text2.text}, 
+			Img: ${newSku_imgCopy.text}, 
+			Retail: ${newSku_retailCopy.text},
+			MinQty: ${newSku_minQtyCopy.text},
+			DiamondQty: ${newSku_DqtyCopy.text},
+			MetalType: ${newSku_metalTypeCopy.selectedOptionValue},
+			Category: ${newSku_categoryCopy.selectedOptionValue},
+			Cost: ${newSku_costCopy.text}
+			`;
+				
+
+				//db event
+				await event_insert.run({
+					event: 'db.update', 
+					event_from: 'appsmith frontend', 
+					event_to: 'bj.sku', 
+					actor: 'employee', 
+					payload: payload_data
+				});
+
+				//biz event
+				await event_insert.run({
+					event: 'business.sku_update', 
+					event_from: 'appsmith frontend', 
+					event_to: 'bj.sku', 
+					actor: 'employee', 
+					payload: payload_data
+				});
+
+
+				closeModal(sku_edit_modal.name);
+				showAlert(`Updated: ${payload_data}`);
+				console.log(payload_data);
+
+				await sku.run();
+
+
+			}
+			else {
+				showAlert('Wrong Password')
+			}
+
+		}
+
+		catch(e) {
+			console.log(e?.message);
+			throw(e);
+		}
+
 	}
 
 }
