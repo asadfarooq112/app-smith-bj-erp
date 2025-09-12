6382975191847230358f9d0980c154fddab78fc1
AI Shah <asadfarooqmb@gmail.com>
2025-09-12 09:09:34 +0000

    all ui done. sales, sale items, finances db crud left. Invoice pdf left,

--- pages/New Sale/jsobjects/product_manipulation/product_manipulation.js
+++ pages/New Sale/jsobjects/product_manipulation/product_manipulation.js
@@ -1,15 +1,16 @@
 export default {
-	
+
 	initializeArray: () => {
-    storeValue('products', []);
-  },
-	
-//////////////////////////////
+		storeValue('products', []);
+	},
+
+
+	//////////////////////////////
 	pushInArray () {
-	
-		
+
+
 		let products = appsmith.store.products;
-		
+
 		let newProduct = {
 			inventory_id: Table_inventory.triggeredRow.inventory_id,
 			image: Table_inventory.triggeredRow.image,
@@ -22,60 +23,60 @@ export default {
 			retail_price: Table_inventory.triggeredRow.retail_price,
 			offered_price: Table_inventory.triggeredRow.retail_price
 		};
-		
+
 		const exists = products.some(obj => obj.inventory_id === newProduct.inventory_id);
-		
+
 		if (exists) {
 			showAlert('Cannot Add Duplicate Inventory Item');
 		}
-		
-		else {
-			
-		let updateProducts = [...products, newProduct];
-		
-		storeValue("products", updateProducts);
 
-		showAlert(`${Table_inventory.triggeredRow.code}, Size: ${Table_inventory.triggeredRow.size}, Weight: ${Table_inventory.triggeredRow.metal_weight} Added`);
-		
-		console.log(products);
-		
+		else {
+
+			let updateProducts = [...products, newProduct];
+
+			storeValue("products", updateProducts);
+
+			showAlert(`${Table_inventory.triggeredRow.code}, Size: ${Table_inventory.triggeredRow.size}, Weight: ${Table_inventory.triggeredRow.metal_weight} Added`);
+
+			console.log(products);
+
 		}
 	},
-	
-//////////////////////////////
-	
+
+	//////////////////////////////
+
 	deleteFromArray () {
 
 		let products = appsmith.store.products;
-		
+
 		const updatedProducts = products.filter( obj => obj.inventory_id !== List1.triggeredItem.inventory_id);
-		
+
 		storeValue("products", updatedProducts);
-		
+
 	},
-	
-//////////////////////////////
+
+	//////////////////////////////
 
 	updateOfferedPrice () {
-		
-		let products = appsmith.store.products;
-		
-		// Try to use the syntax ->>>>  condition ? if return : else return   as much as you can its simple
-		
-		const updatedProducts = products.map(p =>
-      p.inventory_id === List1.triggeredItem.inventory_id ? { ...p, offered_price: new_offered_price.text } : p
-    );
 
-		
+		let products = appsmith.store.products;
+
+		// Try to use the syntax ->>>>  condition ? if return : else return   as much as you can its simple
+
+		const updatedProducts = products.map(p =>
+																				 p.inventory_id === List1.triggeredItem.inventory_id ? { ...p, offered_price: new_offered_price.text } : p
+																				);
+
+
 		storeValue("products", updatedProducts);
 		console.log(updatedProducts);
 		closeModal(modal_offered_price.name);
 		new_offered_price.setValue("");
-		
-},
-	
-	
-	
+
+	},
+
+
+
 	async myFun2 () {
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
