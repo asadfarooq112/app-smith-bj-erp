8698bbafd819d25f809278680dbe78fb767b13c7
AI Shah <asadfarooqmb@gmail.com>
2025-09-14 12:53:47 +0000

    everythiign completed

---
+++ pages/New Sale/jsobjects/invoice_download/invoice_download.js
@@ -0,0 +1,16 @@
+export default {
+
+	async invoiceDowload () {
+	
+		await invoice_buffer_get_and_email.run();
+		const url = `http://178.156.165.247:8080${invoice_buffer_get_and_email.data.url_path}`;
+		
+		const fileName = url.split('tmp/')[1]; // Extract the filename from the URL
+		
+		console.log(url);
+		
+    // Trigger the download
+    await download(url, fileName);
+		
+	}
+}
