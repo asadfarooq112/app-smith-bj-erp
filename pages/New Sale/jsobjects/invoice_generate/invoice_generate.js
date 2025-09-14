08b671045bc8ebb3a4a0ce535aae38af03621e11
AI Shah <asadfarooqmb@gmail.com>
2025-09-14 11:03:00 +0000

    follwoups added. invoice download reminaing only

--- pages/New Sale/jsobjects/invoice_generate/invoice_generate.js
+++ pages/New Sale/jsobjects/invoice_generate/invoice_generate.js
@@ -1,8 +1,9 @@
 export default {
 
-	async myFun2 () {
-
-		
-		
+	downloadPDF: async () => {
+		await invoice_buffer_get_and_email.run();  
+		let data = invoice_buffer_get_and_email.data; // getPdf is your API query
+		const blob = new Blob([data], { type: 'application/pdf' });
+		await download(blob, 'sample.pdf', 'application/pdf');
 	}
 }
